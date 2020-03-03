44d4a111789238577c48bc523b4eeb0c821d657c
John Boyle <nikolasboyle@gmail.com>
2020-03-02 17:34:48 -0800

    feat(user): add fetch route to user api

--- routes/user.js
+++ routes/user.js
@@ -73,6 +73,15 @@ userRouter.post("/sign-in", async (req, res, next) => {
 	}
 });
 
+userRouter.get("/:id", middleware.verifyToken, async (req, res, next) => {
+	try {
+		const user = await User.findById(req.params.id);
+		res.status(200).json(user);
+	} catch (error) {
+		next(error);
+	}
+});
+
 userRouter.put("/", middleware.verifyToken, async (req, res, next) => {
 	try {
 		const user = await User.findByIdAndUpdate(req.userId, req.body, {
